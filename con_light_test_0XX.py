fe3f7cc985c81fc39d4bed02ffd007ce32d06856
fantasticsquirrel <95108850+fantasticsquirrel@users.noreply.github.com>
2021-12-18 10:21:42 -0600

    Update con_light_test_0XX.py

--- con_light_test_0XX.py
+++ con_light_test_0XX.py
@@ -11,12 +11,6 @@ random.seed()
 def seed():
     metadata['operator'] = ctx.caller
 
-    cstl_contract.set('con_castle')
-    fort_contract.set('con_fortress')
-
-    data['cstl_staked_wallets'] = {}
-    data['fort_staked_wallets'] = {}
-
     #prize calclulation Parameters
     metadata['winner_percent'] = decimal('0.09')
     metadata['house_percent'] = decimal('0.01')
@@ -87,14 +81,20 @@ def seed():
         "HI": 67
     }
 
-    #Dark units per castle
-
+    #Dark units per fort
     metadata['UNITS_PER_FORT']={
         "GO": 333,
         "OA": 71,
         "OR": 100
     }
 
+    cstl_contract.set('con_castle')
+    fort_contract.set('con_fortress')
+
+    data['cstl_staked_wallets'] = {}
+    data['fort_staked_wallets'] = {}
+
+    metadata['CSTL_FORT_PER_BATTLE'] = 100
 
 @export
 def change_metadata(key: str, new_value: str, convert_to_decimal: bool=False):
@@ -106,30 +106,33 @@ def change_metadata(key: str, new_value: str, convert_to_decimal: bool=False):
 @export
 def update_units_factors():
 
+    factorA = metadata['factorA']
+    factorB = metadata['factorB']
+
     #LIGHT Unit Parameter factors
-    IN_MDF = metadata['factorA'] * metadata['IN_MD'] + (metadata['factorB'] * metadata['IN_MD']) ** 3
-    IN_RDF = metadata['factorA'] * metadata['IN_RD'] + (metadata['factorB'] * metadata['IN_RD']) ** 3
+    IN_MDF = factorA * metadata['IN_MD'] + (factorB * metadata['IN_MD']) ** 3
+    IN_RDF = factorA * metadata['IN_RD'] + (factorB * metadata['IN_RD']) ** 3
     #IN_WEAKNESS_1 =
-    AR_MDF = metadata['factorA'] * metadata['AR_MD'] + (metadata['factorB'] * metadata['AR_MD']) ** 3
-    AR_RDF = metadata['factorA'] * metadata['AR_RD'] + (metadata['factorB'] * metadata['AR_RD']) ** 3
+    AR_MDF = factorA * metadata['AR_MD'] + (factorB * metadata['AR_MD']) ** 3
+    AR_RDF = factorA * metadata['AR_RD'] + (factorB * metadata['AR_RD']) ** 3
 
-    HI_MDF = metadata['factorA'] * metadata['HI_MD'] + (metadata['factorB'] * metadata['HI_MD']) ** 3
-    HI_RDF = metadata['factorA'] * metadata['HI_RD'] + (metadata['factorB'] * metadata['HI_RD']) ** 3
+    HI_MDF = factorA * metadata['HI_MD'] + (factorB * metadata['HI_MD']) ** 3
+    HI_RDF = factorA * metadata['HI_RD'] + (factorB * metadata['HI_RD']) ** 3
 
 
-    calc['IN','PARAM'] = [metadata['IN_MS'], metadata['IN_MD'], metadata['IN_RS'], metadata['IN_RD'], IN_MDF, IN_RDF, 0.0] #ADD STRENGTHS ANDWEAKNESSES TO UNIT PARAMS
-    calc['AR','PARAM'] = [metadata['AR_MS'], metadata['AR_MD'], metadata['AR_RS'], metadata['AR_RD'], AR_MDF, AR_RDF, 0.0] #ADD STRENGTHS ANDWEAKNESSES TO UNIT PARAMS
-    calc['HI','PARAM'] = [metadata['HI_MS'], metadata['HI_MD'], metadata['HI_RS'], metadata['HI_RD'], HI_MDF, HI_RDF, 0.0] #ADD STRENGTHS ANDWEAKNESSES TO UNIT PARAMS
+    calc['IN','PARAM'] = [metadata['IN_MS'], metadata['IN_MD'], metadata['IN_RS'], metadata['IN_RD'], IN_MDF, IN_RDF, 0.0] #ADD STRENGTHS AND WEAKNESSES TO UNIT PARAMS
+    calc['AR','PARAM'] = [metadata['AR_MS'], metadata['AR_MD'], metadata['AR_RS'], metadata['AR_RD'], AR_MDF, AR_RDF, 0.0] #ADD STRENGTHS AND WEAKNESSES TO UNIT PARAMS
+    calc['HI','PARAM'] = [metadata['HI_MS'], metadata['HI_MD'], metadata['HI_RS'], metadata['HI_RD'], HI_MDF, HI_RDF, 0.0] #ADD STRENGTHS AND WEAKNESSES TO UNIT PARAMS
 
     #DARK Unit Parameter factors
-    GO_MDF = metadata['factorA'] * metadata['GO_MD'] + (metadata['factorB'] * metadata['GO_MD']) ** 3
-    GO_RDF = metadata['factorA'] * metadata['GO_RD'] + (metadata['factorB'] * metadata['GO_RD']) ** 3
+    GO_MDF = factorA * metadata['GO_MD'] + (factorB * metadata['GO_MD']) ** 3
+    GO_RDF = factorA * metadata['GO_RD'] + (factorB * metadata['GO_RD']) ** 3
 
-    OA_MDF = metadata['factorA'] * metadata['OA_MD'] + (metadata['factorB'] * metadata['OA_MD']) ** 3
-    OA_RDF = metadata['factorA'] * metadata['OA_RD'] + (metadata['factorB'] * metadata['OA_RD']) ** 3
+    OA_MDF = factorA * metadata['OA_MD'] + (factorB * metadata['OA_MD']) ** 3
+    OA_RDF = factorA * metadata['OA_RD'] + (factorB * metadata['OA_RD']) ** 3
 
-    OR_MDF = metadata['factorA'] * metadata['OR_MD'] + (metadata['factorB'] * metadata['OR_MD']) ** 3
-    OR_RDF = metadata['factorA'] * metadata['OR_RD'] + (metadata['factorB'] * metadata['OR_RD']) ** 3
+    OR_MDF = factorA * metadata['OR_MD'] + (factorB * metadata['OR_MD']) ** 3
+    OR_RDF = factorA * metadata['OR_RD'] + (factorB * metadata['OR_RD']) ** 3
 
     calc['GO','PARAM'] = [metadata['GO_MS'], metadata['GO_MD'], metadata['GO_RS'], metadata['GO_RD'], GO_MDF, GO_RDF, 0.0]
     calc['OA','PARAM'] = [metadata['OA_MS'], metadata['OA_MD'], metadata['OA_RS'], metadata['OA_RD'], OA_MDF, OA_RDF, 0.0]
@@ -141,7 +144,13 @@ def update_units_factors():
 @export
 def battle():
 
-#ADD LOGIC TO CHECK AND MAKE SURE data['total_cstl'] = data['total_fort'] otherwise don't start battle
+    total_cstl = data['total_cstl']
+    total_fort = data['total_fort']
+    assert total_cstl == total_fort and total_cstl == metadata['CSTL_FORT_PER_BATTLE'], f'There are {total_cstl} CSTL and {total_fort} FORT staked. These must be equal for a battle to be initiated.'
+    operator = metadata['operator']
+    terrains = ['none', 'fields', 'forests', 'hills', 'chaotic']
+    terrain_type = terrains[random.randint(0, 3)]
+    data['terrain_type'] = terrain_type #not needed long term since it's just to see what terrain type was picked.
 
     factor_list = calc['factor_list']
     factorC = factor_list[0]
@@ -160,6 +169,7 @@ def battle():
     OA_PARAM = calc['OA','PARAM']
     OR_PARAM = calc['OR','PARAM']
 
+    #MULTIPLIER to change range vs melee strength through the battle. For now it's set to 1 for the whole battle
     BATTLE_M_MULT = 1
     BATTLE_R_MULT = 1
 
@@ -168,26 +178,29 @@ def battle():
     AR_PARAM[6] = data['AR']
     HI_PARAM[6] = data['HI']
 
-    GO_PARAM[6] = metadata['TEMP_GO']
-    OA_PARAM[6] = metadata['TEMP_OA']
-    OR_PARAM[6] = metadata['TEMP_OR']
-
-    #MULTIPLIER to change range vs melee strength through the battle. For now it's set to 1 for the whole battle
+    GO_PARAM[6] = data['GO']
+    OA_PARAM[6] = data['OA']
+    OR_PARAM[6] = data['OR']
 
     UNITS_TOTAL = calc_army_update(factorC, factorD, BATTLE_M_MULT, BATTLE_R_MULT, IN_PARAM, AR_PARAM, HI_PARAM, GO_PARAM, OA_PARAM, OR_PARAM) #ADD ALL OTHER PARAM LISTS HERE AS UNITS ARE ADDED
 
-    while UNITS_TOTAL[0] > 0 and UNITS_TOTAL[1] > 0:
-        IN_PARAM[6] = calc_losses(IN_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'L', 'D', 'IN', BATTLE_M_MULT, BATTLE_R_MULT) # 3 STRING paremeters are faction of unit, opposing faction, unit name
-        AR_PARAM[6] = calc_losses(AR_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'L', 'D', 'AR', BATTLE_M_MULT, BATTLE_R_MULT) # 3 STRING paremeters are faction of unit, opposing faction, unit name
-        HI_PARAM[6] = calc_losses(HI_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'L', 'D', 'HI', BATTLE_M_MULT, BATTLE_R_MULT) # 3 STRING paremeters are faction of unit, opposing faction, unit name
+    battle_turn = 0
 
-        GO_PARAM[6] = calc_losses(GO_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'D', 'L', 'GO', BATTLE_M_MULT, BATTLE_R_MULT)
-        OA_PARAM[6] = calc_losses(OA_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'D', 'L', 'OA', BATTLE_M_MULT, BATTLE_R_MULT)
-        OR_PARAM[6] = calc_losses(OR_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'D', 'L', 'OR', BATTLE_M_MULT, BATTLE_R_MULT)
+    while UNITS_TOTAL[0] > 0 and UNITS_TOTAL[1] > 0:
+
+        battle_mult = battle_mult_update(terrain_type, battle_turn)
+
+        IN_PARAM[6] = calc_losses(IN_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'L', 'D', 'IN', battle_mult[0], battle_mult[1]) # 3 STRING paremeters are faction of unit, opposing faction, unit name
+        AR_PARAM[6] = calc_losses(AR_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'L', 'D', 'AR', battle_mult[0], battle_mult[1]) # 3 STRING paremeters are faction of unit, opposing faction, unit name
+        HI_PARAM[6] = calc_losses(HI_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'L', 'D', 'HI', battle_mult[0], battle_mult[1]) # 3 STRING paremeters are faction of unit, opposing faction, unit name
+
+        GO_PARAM[6] = calc_losses(GO_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'D', 'L', 'GO', battle_mult[0], battle_mult[1])
+        OA_PARAM[6] = calc_losses(OA_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'D', 'L', 'OA', battle_mult[0], battle_mult[1])
+        OR_PARAM[6] = calc_losses(OR_PARAM, factorE, multiplier, lower, upper, STR_bonus, 'D', 'L', 'OR', battle_mult[0], battle_mult[1])
 
         UNITS_TOTAL = calc_army_update(factorC, factorD, BATTLE_M_MULT, BATTLE_R_MULT, IN_PARAM, AR_PARAM, HI_PARAM, GO_PARAM, OA_PARAM, OR_PARAM) #ADD ALL OTHER PARAM LISTS HERE AS UNITS ARE ADDED
+        battle_turn += 1
 
-    calc['Battle_Results'] = f'There are {int(IN_PARAM[6])} infantry, {int(AR_PARAM[6])} archers, and {int(HI_PARAM[6])} heavy infantry remaining in the LIGHT army, and there are {int(GO_PARAM[6])} goblins, {int(OA_PARAM[6])} orc archers, and {int(OR_PARAM[6])} orcs remaining in the DARK army.'
     if UNITS_TOTAL[0] > 0 and UNITS_TOTAL[1] <= 0:
         winner = 'L'
     elif UNITS_TOTAL[1] > 0 and UNITS_TOTAL[0] <= 0:
@@ -195,12 +208,51 @@ def battle():
     else:
         winner = 'error'
 
-
-
-    disperse(winner)
+    disperse(operator, winner)
 
     data['cstl_staked_wallets'].clear() #clears all staked wallets from storage so a new battle can start
     data['fort_staked_wallets'].clear()
+
+    data['IN'] = 0 #add all other units here as they're added
+    data['AR'] = 0
+    data['HI'] = 0
+
+    data['GO'] = 0
+    data['OA'] = 0
+    data['OR'] = 0
+
+def battle_mult_update(terrain_type, battle_turn): # ['none', 'fields', 'forests', 'hills', 'chaotic']
+    if terrain_type == 'none':
+        battle_m_mult = 1
+        battle_r_mult = 1
+
+    if terrain_type == 'fields':
+        battle_m_mult = battle_turn * 0.1 + 0.25
+        battle_r_mult = 1 - (battle_turn * 0.1)
+
+    if terrain_type == 'forests':
+        battle_m_mult = 1 - (battle_turn * 0.1)
+        battle_r_mult = battle_turn * 0.1 + 0.25
+
+    if terrain_type == 'hills':
+        if (battle_turn % 2) == 0:
+            battle_m_mult = 0.75 - (battle_turn * 0.1)
+            battle_r_mult = 0.75 + (battle_turn * 0.1)
+        else:
+            battle_m_mult = 0.75 + (battle_turn * 0.1)
+            battle_r_mult = 0.75 - (battle_turn * 0.1)
+
+    if terrain_type == 'chaotic':
+        battle_m_mult = random.randint(0, 100) * 0.01
+        battle_r_mult = random.randint(0, 100) * 0.01
+
+    if battle_m_mult > 1 : battle_m_mult = 1
+    if battle_m_mult < 0.25 : battle_m_mult = 0.25
+    if battle_r_mult > 1 : battle_r_mult = 1
+    if battle_r_mult < 0.25 : battle_r_mult = 0.25
+
+    battle_mult = [battle_m_mult, battle_r_mult]
+    return battle_mult
 
 @export
 def calc_losses(unit_param: float, factorE: float, multiplier: float, lower : float, upper: float, STR_bonus: float, faction_unit: str, faction_other: str, unit_type: str, BATTLE_M_MULT: float, BATTLE_R_MULT: float, Mweak1: str='PLACEHOLDER', Mweak1count: float=0, Mweak2: str='PLACEHOLDER', Mweak2count: float=0,Rweak1: str='PLACEHOLDER', Rweak1count: float=0,Rweak2: str='PLACEHOLDER',Rweak2count: float=0):
@@ -219,7 +271,6 @@ def calc_losses(unit_param: float, factorE: float, multiplier: float, lower : fl
 
 @export
 def calc_army_update(factorC: float, factorD: float, BATTLE_M_MULT: float, BATTLE_R_MULT: float, IN_PARAM: float, AR_PARAM: float, HI_PARAM: float, GO_PARAM: float, OA_PARAM: float, OR_PARAM: float): #ADD ALL OTHER PARAM LISTS HERE AS UNITS ARE ADDED
-
 
     L_UNITS_IN = IN_PARAM[6]
     L_UNITS_AR = AR_PARAM[6]
@@ -265,10 +316,8 @@ def calc_army_update(factorC: float, factorD: float, BATTLE_M_MULT: float, BATTL
 @export
 def stake_CSTL(cstl_amount: int, IN_CSTL: float, AR_CSTL: float, HI_CSTL: float):
 
-assert IN_CSTL + AR_CSTL + HI_CSTL == cstl_amount, "total number of CSTL must equal the sum of the CSTL used to train each unit"
-assert
-#put error checking total number of castles already in contract vs total possible for the battle.
-#put error about more castle types than total castles.
+    assert IN_CSTL + AR_CSTL + HI_CSTL == cstl_amount, "Total number of CSTL must equal the sum of the CSTL used to train each unit."
+    assert data['total_cstl'] + cstl_amount <= metadata['CSTL_FORT_PER_BATTLE'], f'You are attempting to stake {cstl_amount} which is more than the {metadata["CSTL_FORT_PER_BATTLE"] - data["total_cstl"]} remaining to be staked for this battle. Please try again with a smaller number.'
 #put error checking to see if a battle has been started.
 #copy this whole function but make it for FORT
 
@@ -295,8 +344,38 @@ assert
     data['AR'] += AR_amount
     data['HI'] += HI_amount
 
+@export
+def stake_FORT(fort_amount: int, GO_FORT: float, OA_FORT: float, OR_FORT: float):
 
-def disperse(winner: str):
+    assert GO_FORT + OA_FORT + OR_FORT == fort_amount, "Total number of FORT must equal the sum of the FORT used to train each unit."
+    assert data['total_fort'] + fort_amount <= metadata['CSTL_FORT_PER_BATTLE'], f'You are attempting to stake {fort_amount} which is more than the {metadata["CSTL_FORT_PER_BATTLE"] - data["total_fort"]} remaining to be staked for this battle. Please try again with a smaller number.'
+#put error checking to see if a battle has been started.
+#copy this whole function but make it for FORT
+
+    staked_wallets = data['fort_staked_wallets']
+    fort = importlib.import_module(fort_contract.get())
+
+    UNITS_PER_FORT = metadata['UNITS_PER_FORT']
+
+    GO_amount = UNITS_PER_FORT["GO"] * GO_FORT
+    OA_amount = UNITS_PER_FORT["OA"] * OA_FORT
+    OR_amount = UNITS_PER_FORT["OR"] * OR_FORT
+
+    fort.transfer_from(amount=fort_amount, to=ctx.this, main_account=ctx.caller)
+
+    if ctx.caller not in staked_wallets:
+        staked_wallets.update({ctx.caller: fort_amount})
+    else:
+        staked_wallets[ctx.caller] += fort_amount
+
+    data['fort_staked_wallets'] = staked_wallets #adds the staker to the dict for calculating rewards for winners and losers
+    data['total_fort'] += fort_amount #adds total FORT to storage for calculating rewards
+
+    data['GO'] += GO_amount
+    data['OA'] += OA_amount
+    data['OR'] += OR_amount
+
+def disperse(operator: str, winner: str):
     #calculate winnings where winners get 1.09, loser gets 0.90 and house gets 0.01
 
     cstl = importlib.import_module(cstl_contract.get())
@@ -312,20 +391,20 @@ def disperse(winner: str):
         #send winnings to CASTLE stakers
         for key, value in dict(cstl_staked_wallets).items():
             cstl.transfer(amount=value, to=key)
-            fort.transfer(amount=value * winner_percent, to=key)
-            fort.transfer(amount=value * house_percent, to=OWNER) #make variable for OWNER
+            fort.transfer(amount= (value * winner_percent), to=key)
+            fort.transfer(amount= (value * house_percent), to=operator) #make variable for OWNER
 
         for key, value in dict(fort_staked_wallets).items():
-            fort.transfer(amount=value * loser_percent, to=key)
+            fort.transfer(amount= (value * loser_percent), to=key)
     if winner == 'D':
         #send winnings to FORTRESS stakers
         for key, value in dict(fort_staked_wallets).items():
             fort.transfer(amount=value, to=key)
-            cstl.transfer(amount=value * winner_percent, to=key)
-            cstl.transfer(amount=value * house_percent, to=OWNER)
+            cstl.transfer(amount= (value * winner_percent), to=key)
+            cstl.transfer(amount= (value * house_percent), to=operator)
 
         for key, value in dict(cstl_staked_wallets).items():
-            cstl.transfer(amount=value * loser_percent, to=key)
+            cstl.transfer(amount= (value * loser_percent), to=key)
 
 
 
@@ -337,6 +416,11 @@ def disperse(winner: str):
 
 
 
+
+
+
+
+#
 
 
 
